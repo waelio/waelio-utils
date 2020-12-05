b7afceb15ec5ab4d13ff3e6b64b250fe74a8d6b5
Wael wahbeh <waelio@users.noreply.github.com>
2020-12-05 11:21:59 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# waelio-utils
-Helper Utils often used in all waelio websites
+# NorthWestMeta Utilities
+### Helper Utils often used in all waelio websites
+### _usage_: `npm install --save waelio-utils`
+### USAGE: In your Javascript file:
+```javascript
+import { JSONToQueryString } from 'waelio-utils'
+const objectParams = { first: 'John', last: 'Smith' }
+const queryParams = JSONToQueryString( objectParams )
+````
+### queryParams `name=John&last=smith`
+#
+
+## JSONToQueryString
+### Function that converts a JSON to URL Query String
+#### Example IN: `{"first":"John", "last": "Smith"}`
+#### Example Out: `first=John&last=Smith`
+#### @param `{}` JSON payload
+### _Returns_ `String`
+#
+## QueryStringToJSON
+### Function that converts a URL Query String to JSON
+#### Example IN: `first=John&last=Smith`
+#### Example Out: `{"first":"John", "last": "Smith"}`
+#### @param `{string}` payload QueryString
+#### @param `{boolean}` toObject Return JS Object or JSON
+### _Returns_ `JSON || Object`
+#
+## resetString
+### _Returns_ `decodeURIComponent( encodeURIComponent( payload ) )`
+#
+## snakeToCamel
+### Function that converts snake_case or snake-case to camelCase "snakeCase"
+#### Example IN: `snake_case`
+#### Example Out: `snakeCase`
+#### @name  snakeToCamel
+#### @param {string} payload QueryString
+### _Returns_ `{string}`
+# 
+## Base64
+### Convert string to `Base64`
+#
+## reParseString
+ ### _Returns_ `JSON.parse(JSON.stringify(payload))`
+#
+## generateId
+### Generate random string/id
+#### @param  {number} start `2` default
+#### @param  {number} len   `9` default
+### _Returns_ `{string}`
+#git remote add origin git://waelio-utils
---
+++ index.js
@@ -0,0 +1,94 @@
+
+/**
+ * Function that converts a JSON to URL Query String
+ * Example IN: {"first":"John", "last": "Smith"}
+ * Example Out: first=John&last=Smith
+ * @name  JSONToQueryString
+ * @author  Wael Wahbeh <wahbehw@gmail.com>
+ * @function
+ * @global
+ * @param {} -JSON payload
+ * @returns QueryString 
+ */
+export function JSONToQueryString (payload) {
+  payload = JSON.parse(JSON.stringify(payload))
+  return Object.keys(payload)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(payload[key])}`).join('&')
+}
+
+/**
+ * Function that converts a URL Query String to JSON
+ * Example IN: first=John&last=Smith
+ * Example Out: {"first":"John", "last": "Smith"}
+ * @name  QueryStringToJSON
+ * @author  Wael Wahbeh <wahbehw@gmail.com>
+ * @function
+ * @global
+ * @param {string} payload QueryString
+ * @param {boolean} toObject Return JS Object or JSON
+ * @returns JSON|Object
+ */
+export function QueryStringToJSON (payload, toObject = true) {
+  if (!payload) return
+  var pairs = payload.slice(1).split('&')
+  var result = {}
+  pairs.forEach(pair => {
+    pair = pair.split('=')
+    result[pair[0]] = decodeURIComponent(pair[1] || '')
+  })
+  return toObject ? JSON.parse(JSON.stringify(result)) : JSON.stringify(result)
+}
+export function resetString (payload) {
+  return decodeURIComponent(encodeURIComponent(payload))
+}
+
+/**
+ * Function that converts snake_case or snake-case to camelCase "snakeCase"
+ * Example IN: snake_case
+ * Example Out: snakeCase
+ * @name  snakeToCamel
+ * @author  Wael Wahbeh <wahbehw@gmail.com>
+ * @function
+ * @global
+ * @param {string} payload QueryString
+ * @returns {string}
+ */
+export function snakeToCamel (payload) {
+  return (typeof payload !== 'string') ? payload : payload.replace(/([-_]\w)/g, g => g[1].toUpperCase())
+}
+
+/**
+ * Calculate Clock Drift used to calculate tile remaining before token expiration
+ *  
+ * @param  {} iatAccessToken IAT 
+ * @param  {} iatIdToken
+ */
+export function calculateClockDrift (iatAccessToken, iatIdToken) {
+  const now = Math.floor(new Date() / 1000)
+  const iat = Math.min(iatAccessToken, iatIdToken)
+  return now - iat
+}
+
+
+/**
+ * Converts string to a Base64
+ * @param  {string} payload
+ */
+export async function Base64 (payload) {
+  return await btoa(unescape(encodeURIComponent(payload)))
+}
+
+export async function reParseString (payload) {
+  return await JSON.parse(JSON.stringify(await payload))
+}
+
+/** generate random string
+ * @name  generateId
+ * @author  Wael Wahbeh <wahbehw@gmail.com>
+ * @param  {number} start 2 default
+ * @param  {number} len   9 default
+ * @return {string}
+ */
+export async unction generateId (start = 2, len = 9) { 
+  return Math.random().toString(36).substr(start, len)
+}

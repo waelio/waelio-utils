ea5fdefc6078e20683299aea67b89bd4531aebe8
Wael wahbeh <waelio@users.noreply.github.com>
2021-04-07 10:15:50 +0300

    revert to basic utils

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-import app from './src';
+import { sniffId, meta, notifyMe, generateId, reParseString, Base64, calculateClockDrift, isObject, isArray, camelToSnake, snakeToCamel, resetString, queryStringToJson, jsonToQueryString } from './src/utils';
 
-export default app
+export { sniffId, meta, notifyMe, generateId, reParseString, Base64, calculateClockDrift, isObject, isArray, camelToSnake, snakeToCamel, resetString, queryStringToJson, jsonToQueryString }
+export default {
+  input: './src/utils',
+  output: 'dist/waelioUtils.js',
+  format: 'esm',
+  exports: 'named', /** Disable warning for default imports */
+  sourcemap: true,
+};
--- rollup.config.js
+++ rollup.config.js
@@ -3,54 +3,23 @@ import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default 
-  [{
-    input: 'src/plugins',
-    plugins: [ resolve(),json(),commonjs()],
-    output: {
-      file: 'dist/plugins.js',
-      format: 'es'
-    }
-  },
-  {
-    input: 'src/plugins/config',
-    plugins: [ resolve(),json(),commonjs()],
-    output: {
-      file: 'dist/config.js',
-      format: 'cjs'
-    }
-  },
-  {
-    input: 'src/plugins/note',
-    plugins: [ resolve(),json(),commonjs()],
-    output: {
-      file: 'dist/note.js',
-      format: 'es'
-    }
-  },
-  {
-    input: 'src/plugins/store',
-    plugins: [ resolve(),json(),commonjs()],
-    output: {
-      file: 'dist/store.js',
-      format: 'es'
-    }
-  },
+  [
   {
     input: 'src/utils',
     plugins: [ resolve(),json(),commonjs()],
     output: [
       {
-        file: 'dist/waelioUtils.js',
+        file: 'dist/waelioUtils.cjs',
         format: 'cjs'
       },
       {
-        file: 'dist/waelioUtils-umd.js',
+        file: 'dist/waelioUtils.js',
         name: 'waelioUtils',
         format: 'umd'
       },
       {
-        file: 'dist/waelioUtils-es.js',
-        format: 'es'
+        file: 'dist/waelioUtils.ejs',
+        format: 'esm'
       }
     ]
   }  
